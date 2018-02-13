4831d57fc223494ee741da0d5002a0d34c98fe38
Madeline Stark <madeline.stark1@gmail.com>
2018-02-13 21:24:17 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(int)
-  if fizzbuzz(int) % 3 == 0
-    "Fizz"
+  if fizzbuzz(int) % 3 == 0 && fizzbuzz(int) % 5 == 0
+    "FizzBuzz"
+  elsif fizzbuzz(int) % 3 == 0
+  "Fizz"
   elsif fizzbuzz(int) % 5 == 0
     "Buzz"
-  elsif fizzbuzz(int) % 3 == 0 && fizzbuzz(int) % 5 == 0
-    "FizzBuzz"
   else
     nil
   end
